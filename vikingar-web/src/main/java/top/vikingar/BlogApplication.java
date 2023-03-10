f5bcef5882db7b66e1e0cc2464f9de663e314482
vikingar <423141759@qq.com>
2023-03-11 00:43:01 +0800

    init

---
+++ vikingar-web/src/main/java/top/vikingar/BlogApplication.java
@@ -0,0 +1,15 @@
+package top.vikingar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author vikingar
+ * @time 2023/3/11 0:42
+ */
+@SpringBootApplication
+public class BlogApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BlogApplication.class, args);
+    }
+}
